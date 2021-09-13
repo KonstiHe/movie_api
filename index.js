6833da2d040ac3ffa0774dc7318a7e603b329306
KonstiHe <79403490+KonstiHe@users.noreply.github.com>
2021-09-13 12:16:08 +0200

    creating endpoints

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const uuid = require("uuid");
 const app = express();
 
 let topMovies = [
-  { title: "", regie: "" },
+  { title: "Kostja", regie: "" },
   { title: "", regie: "" },
   { title: "", regie: "" },
   { title: "", regie: "" },
@@ -16,6 +18,7 @@ let topMovies = [
 ];
 
 app.use(morgan("common"));
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.send("Welcome to my favorite movies!");
@@ -25,6 +28,43 @@ app.get("/movies", (req, res) => {
   res.json(topMovies);
 });
 
+app.get("/movies/:title", (req, res) => {
+  res.send("Successful GET request returning data on a title");
+});
+
+app.get("/genres/:genre", (req, res) => {
+  res.send("Successful GET request returning data on a genre");
+});
+
+app.get("/directors/:name", (req, res) => {
+  res.send("Successful GET request returning data on a director.");
+});
+
+app.post("/register", (req, res) => {
+  res.send("Successful POST request returning data of a new user.");
+});
+
+app.put("/users/:name", (req, res) => {
+  res.send("Successful PUT request returning updated data of a user.");
+});
+
+app.put("/users/:name/favorites/:newMovie", (req, res) => {
+  res.send(
+    "Successful PUT request returning a message to a user about successufully adding a movie."
+  );
+});
+
+app.delete("/users/:name/favorites/:movieToRemove", (req, res) => {
+  res.send(
+    "Successful DELETE request returning a message to a user about successufully deleting a movie."
+  );
+});
+
+app.delete("/users/:name/deregister", (req, res) => {
+  res.send(
+    "Successful DELETE request returning a message to a user about successufully deregistering."
+  );
+});
 app.use(express.static("public"));
 
 app.use((err, req, res, next) => {
